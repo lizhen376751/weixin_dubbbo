58fd18f8fea572b89b0303f719f8d5e5412d2be0
miaohao <8166405@qq.com>
2017-03-27 09:40:29 +0800

    初始化提交

---
+++ DuduWeixin-dubbo-web/src/test/java/com/dudu/soa/wxd/test/TestBase.java
@@ -0,0 +1,26 @@
+package com.dudu.soa.wxd.test;
+
+
+import org.junit.runner.RunWith;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringJUnit4ClassRunner.class)  
+@ContextConfiguration("classpath*:conf/*-spring.xml") 
+public abstract class TestBase {
+	
+	@Resource
+	ApplicationContext ctx;
+
+	public void init(){
+		try {
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
+}
