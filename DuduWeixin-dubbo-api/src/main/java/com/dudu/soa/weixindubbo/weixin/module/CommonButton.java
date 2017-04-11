954cc83a5389ec09e9b4a1751bd87723e1786421
lizhen <724577807@qq.com>
2017-04-11 18:41:44 +0800

    见天按钮的实体类

---
+++ DuduWeixin-dubbo-api/src/main/java/com/dudu/soa/weixindubbo/weixin/module/CommonButton.java
@@ -0,0 +1,34 @@
+package com.dudu.soa.weixindubbo.weixin.module;
+
+
+
+
+public class CommonButton extends Button {
+	private String type;
+	private String key;
+	private String url;
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+}
