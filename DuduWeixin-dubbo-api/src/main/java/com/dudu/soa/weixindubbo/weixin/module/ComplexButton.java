932ac8e8c34a1a0105a1386e317eba103a77c3c0
lizhen <724577807@qq.com>
2017-04-11 18:43:34 +0800

    添加按钮的实体类

---
+++ DuduWeixin-dubbo-api/src/main/java/com/dudu/soa/weixindubbo/weixin/module/ComplexButton.java
@@ -0,0 +1,18 @@
+package com.dudu.soa.weixindubbo.weixin.module;
+
+
+
+/**
+ * ���Ӱ�ť������ť��
+ * 
+ */
+public class ComplexButton extends Button {
+	private Button[] sub_button;
+	public Button[] getSub_button() {
+		return sub_button;
+	}
+
+	public void setSub_button(Button[] sub_button) {
+		this.sub_button = sub_button;
+	}
+}
