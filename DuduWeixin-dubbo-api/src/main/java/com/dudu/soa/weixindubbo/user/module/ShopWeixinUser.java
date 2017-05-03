0e6a6b1d5b62249e0e7742d4ea9fcb2d366c70da
lizhen <724577807@qq.com>
2017-05-03 18:10:17 +0800

    店管家微信用户接口的修改

---
+++ DuduWeixin-dubbo-api/src/main/java/com/dudu/soa/weixindubbo/user/module/ShopWeixinUser.java
@@ -0,0 +1,49 @@
+package com.dudu.soa.weixindubbo.user.module;
+
+import java.io.Serializable;
+
+/**
+ * Created by lizhen on 2017/3/29.
+ */
+
+public class ShopWeixinUser implements Serializable{
+    private int ID;
+    private String UserName;
+    private String UserPass;
+    private String Mobile;
+    private String Address;
+    private String Engine;
+    private String IDCode;
+    private String RegistCode;
+    private String RecordDate;
+    private String Brand;
+    private String Serise;
+    private String City;
+    private String State;
+    private String RegisterTime;
+    private String city_code;
+    private String OpenId;
+    private String OpenU;
+    private String shopcode;
+    /**
+     * 姓名
+     */
+    private String xingMing;
+    /**
+     * 性别
+     */
+    private String xingBie;
+    /**
+     * 车排量
+     */
+    private String carPaiLiang;
+    /**
+     * 微信昵称
+     */
+    private String nickname;
+    /**
+     * 用户的关注时间
+     */
+    private String subscribetime;
+
+}
