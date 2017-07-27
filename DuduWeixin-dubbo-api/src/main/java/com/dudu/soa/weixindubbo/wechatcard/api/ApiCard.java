18d109560daba9cc1de356724ef9eb1df9ce3e2b
lizhen <724577807@qq.com>
2017-07-27 16:20:57 +0800

    微信卡券

---
+++ DuduWeixin-dubbo-api/src/main/java/com/dudu/soa/weixindubbo/wechatcard/api/ApiCard.java
@@ -0,0 +1,22 @@
+package com.dudu.soa.weixindubbo.wechatcard.api;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+
+/**
+ * 微信卡包相關接口
+ * Created by lizhen on 2017/7/27.
+ */
+
+public interface ApiCard {
+    /**
+     * 模拟form表单的形式 ，上传文件 以输出流的形式把文件写入到url中，然后用输入流来获取url的响应
+     *
+     * @param token    请求地址 form表单url地址
+     * @param filePath 文件的
+     * @param in       文件的
+     * @return String url的响应信息返回值
+     * @throws IOException 异常
+     */
+     String send(String token, String filePath, DataInputStream in);
+}
